e3ce4b850fecadb4dad79ef68aa7b3628d9180f3
Tejaspatil1175 <tejaspatil1175@gmail.com>
2026-03-03 19:14:13 +0530

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -118,7 +118,7 @@ const App = () => {
         <Analytics />
         <BrowserRouter>
           <Routes>
-            <Route path="/dk" element={<Index />} />
+            <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
